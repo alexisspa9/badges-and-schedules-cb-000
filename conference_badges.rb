79b02759bbf5f0db0402d5ce3e69574386b1b548
Alexander Spanos <alexisspa9@gmail.com>
2018-03-22 09:07:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(array)
   array_messages =[]
   array.each{|name| array_messages.push(badge_maker(name))}
   array_messages
